a0e3b49e3c4db20c8d6ad181a353d7bc32ebfd7c
streaming-yassine <44115214+MyassineM@users.noreply.github.com>
2018-11-30 11:38:25 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "p";
-var adminprefix = 'p'
+var prefix = "+";
+var adminprefix = '+'
 const developers = ["436918120184021012"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
